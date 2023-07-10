0c4013a96eaab58539f6ae22471c4b43326349bf
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-07-10 12:58:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ document.getElementById("incrBtn").addEventListener("click", function() {
   xhr.open("POST", "save.php", true);
   xhr.send();
 });
+
+var incrBtn = document.getElementById("incrBtn");
+var numDisplay = document.getElementById("numDisplay");
+
+var number = 0;
+
+incrBtn.addEventListener("click", function() {
+  number++;
+
+  numDisplay.textContent = number;
+});
